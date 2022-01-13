245cece59857b2347149892d844c9e9c47b49cef
dwashi21 <dwashi21@gmail.com>
2022-01-13 01:47:14 -0600

    Seed Data route works

--- controllers/toonsController.js
+++ controllers/toonsController.js
@@ -87,37 +87,52 @@ router.put('/:id', (req, res) => {
 
 
 //SEED 
-// router.get('/seed', async (req, res) => {
-//     const newtoons =
-//       [
-//         {
-//           name: 'Beans',
-//           description: 'A small pile of beans. Buy more beans for a big pile of beans.',
-//           img: 'https://cdn3.bigcommerce.com/s-a6pgxdjc7w/toons/1075/images/967/416130__50605.1467418920.1280.1280.jpg?c=2',
-//           price: 5,
-//           qty: 99
-//         }, {
-//           name: 'Bones',
-//           description: 'It\'s just a bag of bones.',
-//           img: 'http://bluelips.com/prod_images_large/bones1.jpg',
-//           price: 25,
-//           qty: 0
-//         }, {
-//           name: 'Bins',
-//           description: 'A stack of colorful bins for your beans and bones.',
-//           img: 'http://www.clipartbest.com/cliparts/9cz/rMM/9czrMMBcE.jpeg',
-//           price: 7000,
-//           qty: 1
-//         }
-//       ]
+router.get('/seed', async (req, res) => {
+    const newToons =
+      [
+        {
+        class: "Warrior",
+        name: "John Smith",
+        description: "A valiant fighter of unmatched strength",
+        img: "",
+        str: 14,
+        dex: 10,
+        con: 17,
+        int: 8,
+        wis: 10,
+        charm: 12
+        }, {
+        class: "Rogue",
+        name: 'Dusty Bones',
+        description: 'A shadowy figure with a stealthy approach',
+        img: "",
+        str: 12,
+        dex: 18,
+        con: 12,
+        int: 8,
+        wis: 10,
+        charm: 16
+        }, {
+        class: "Sorcerer",
+        name: 'Jaina Menethil',
+        description: 'Sorceress Supreme, big attitude bigger spells',
+        img: "",
+        str: 6,
+        dex: 9,
+        con: 14,
+        int: 8,
+        wis: 12,
+        charm: 17
+        }
+      ]
   
-//     try {
-//       const seedItems = await Toon.create(newtoons)
-//       res.send(seedItems)
-//     } catch (err) {
-//       res.send(err.message)
-//     }
-//   })
+    try {
+      const seedItems = await Toon.create(newToons)
+      res.send(seedItems)
+    } catch (err) {
+      res.send(err.message)
+    }
+  })
 
   //SHOW
 router.get('/:id', (req, res) => {
